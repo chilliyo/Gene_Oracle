dd21b20e994fadf0aff8f607f674c9f3520d2be0
Carissa Ward <carissaward@carissas-macbook-pro.local>
2017-03-05 17:42:49 -0600

    Initial Commit

---
+++ Carissa_Ward_and_Qili_Sui_Gene_Oracle/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Carissa_Ward_and_Qili_Sui_Gene_Oracle
+//
+//  Created by Carissa Ward on 3/5/17.
+//  Copyright © 2017 DePaul. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

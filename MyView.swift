cf40d7134619c75131897c83f92a678fe293bd42
Carissa Ward <carissaward@Carissas-MacBook-Pro.local>
2017-03-15 10:52:40 -0500

    some drawing work

--- MyView.swift
+++ MyView.swift
@@ -14,30 +14,7 @@ class MyView: UIView {
         // Drawing code
         
         if let context = UIGraphicsGetCurrentContext() {
-            
-            // Drawing lines
-            context.move(to: CGPoint(x: 20, y: 40))
-            context.addLine(to: CGPoint(x: 120, y: 90))
-            context.strokePath()
-            
-            context.setLineWidth(4)
-            context.move(to: CGPoint(x: 60, y: 40))
-            context.addLine(to: CGPoint(x: 160, y: 90))
-            context.strokePath()
-            
-            // dashed lines
-            let shortDash : [CGFloat] = [ 4, 4 ]
-            context.setLineDash(phase: 0, lengths: shortDash)
-            context.move(to: CGPoint(x: 100, y: 40))
-            context.addLine(to: CGPoint(x: 200, y: 90))
-            context.strokePath()
-            
-            context.setLineWidth(2)
-            let longDash : [CGFloat] = [ 8, 2 ]
-            context.setLineDash(phase:0, lengths: longDash)
-            context.move(to: CGPoint(x: 140, y: 40))
-            context.addLine(to: CGPoint(x: 240, y: 90))
-            context.strokePath()
+        
             
             // Drawing ovals and circles
             context.setStrokeColor(UIColor.blue.cgColor)
@@ -47,7 +24,7 @@ class MyView: UIView {
             let rect1 = CGRect(x: 20, y: 100, width: 80, height: 60)
             context.strokeEllipse(in: rect1)
             
-            context.setLineWidth(4)
+            context.setLineWidth(8)
             let rect2 = CGRect(x: 120, y: 100, width: 80, height: 60)
             context.fillEllipse(in: rect2)
             context.strokeEllipse(in: rect2)
@@ -57,19 +34,18 @@ class MyView: UIView {
             
             // Drawing rectangles and squares
             context.setStrokeColor(UIColor.green.cgColor)
-            context.setFillColor(UIColor.yellow.cgColor)
+            context.setFillColor(UIColor.blue.cgColor)
             context.setLineWidth(2)
             
             let rect4 = CGRect(x: 20, y: 180, width: 80, height: 60)
             context.stroke(rect4)
             
             context.setLineWidth(4)
-            let rect5 = CGRect(x: 120, y: 180, width: 80, height: 60)
+            let rect5 = CGRect(x: 120, y: 180, width: 60, height: 60)
             context.fill(rect5)
             context.stroke(rect5)
             
-            let rect6 = CGRect(x: 220, y: 180, width: 60, height: 60)
-            context.fill(rect6)
+
             
             // Drawing path
             context.setStrokeColor(UIColor.blue.cgColor)
@@ -90,26 +66,6 @@ class MyView: UIView {
                            clockwise: true)
             context.strokePath()
             
-            
-            // Drawing a quadratic Bézier curve
-            context.move(to: CGPoint(x: 20, y: 350))
-            context.addQuadCurve(to: CGPoint(x: 170, y: 350),
-                                 control: CGPoint(x: 100, y: 450))
-            context.strokePath()
-            
-            // Drawing a cubic Bézier curve
-            context.move(to: CGPoint(x: 170, y: 400))
-            context.addCurve(to: CGPoint(x: 320, y: 350),
-                             control1: CGPoint(x: 220, y: 420),
-                             control2: CGPoint(x: 270, y: 330))
-            context.strokePath()
-            
-            // Drawing an image
-            if let image = UIImage(named: "Chicago") {
-                let rect = CGRect(x: 20, y: 420, width: 150, height: 100)
-                //image.draw(at: CGPoint(x: 20, y: 420))  // draw at full size
-                image.draw(in: rect)
-            }
             
             // Drawing text
             let text =  "Hello Quartz 2D!"

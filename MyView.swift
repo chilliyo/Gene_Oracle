7d44e5392faa8f17cd9d60dd3c41a17c631fcd3b
Carissa Ward <carissaward@Carissas-MacBook-Pro.local>
2017-03-15 12:14:46 -0500

    blood cell and sickle cell drawings complete

--- MyView.swift
+++ MyView.swift
@@ -17,54 +17,70 @@ class MyView: UIView {
         
             
             // Drawing ovals and circles
-            context.setStrokeColor(UIColor.blue.cgColor)
-            context.setFillColor(UIColor.red.cgColor)
+            context.setStrokeColor(UIColor.red.cgColor)
+            context.setFillColor(UIColor(red: 225/255.0, green: 0/255.0, blue: 0/255.0, alpha: 1.0).cgColor)
             context.setLineDash(phase:0, lengths: [])
             
-            let rect1 = CGRect(x: 20, y: 100, width: 80, height: 60)
-            context.strokeEllipse(in: rect1)
             
-            context.setLineWidth(8)
-            let rect2 = CGRect(x: 120, y: 100, width: 80, height: 60)
-            context.fillEllipse(in: rect2)
-            context.strokeEllipse(in: rect2)
+            context.setLineWidth(18)
+            let bloodCell = CGRect(x: 20, y: 100, width: 60, height: 60)
+            context.fillEllipse(in: bloodCell)
+            context.strokeEllipse(in: bloodCell)
+
             
-            let rect3 = CGRect(x: 220, y: 100, width: 60, height: 60)
-            context.fillEllipse(in: rect3)
+            
+            
+            // Drawing an arc
+            context.setLineWidth(10)
+            context.setStrokeColor(UIColor(red: 225/255.0, green: 0/255.0, blue: 0/255.0, alpha: 1.0).cgColor)
+            context.addArc(center: CGPoint(x: 6.5, y: 240.5),
+                           radius: 62.5,
+                           startAngle: 0,
+                           endAngle: CGFloat(M_PI / 0.62),
+                           clockwise: true)
+            context.strokePath()
+
+            
+            context.setStrokeColor(UIColor.red.cgColor)
+            
+            context.addArc(center: CGPoint(x: 23, y: 230),
+                           radius: 50,
+                           startAngle: CGFloat(M_PI / 6.0),
+                           endAngle: CGFloat(M_PI / 0.666),
+                           clockwise: true)
+            context.strokePath()
+            
+            context.setLineWidth(5)
+            // Drawing an arc
+            context.addArc(center: CGPoint(x: -10, y: 251),
+                           radius: 75,
+                           startAngle: CGFloat(0 * M_PI / 6.0),
+                           endAngle: CGFloat(M_PI / 0.61),
+                           clockwise: true)
+            context.strokePath()
             
             // Drawing rectangles and squares
             context.setStrokeColor(UIColor.green.cgColor)
             context.setFillColor(UIColor.blue.cgColor)
             context.setLineWidth(2)
             
-            let rect4 = CGRect(x: 20, y: 180, width: 80, height: 60)
-            context.stroke(rect4)
-            
             context.setLineWidth(4)
-            let rect5 = CGRect(x: 120, y: 180, width: 60, height: 60)
+            let rect5 = CGRect(x: 120, y: 400, width: 60, height: 60)
             context.fill(rect5)
             context.stroke(rect5)
             
-
-            
             // Drawing path
-            context.setStrokeColor(UIColor.blue.cgColor)
-            context.setLineWidth(2)
+            //context.setStrokeColor(UIColor.blue.cgColor)
+            //context.setLineWidth(2)
             
-            context.move(to: CGPoint(x: 100, y: 250))
-            context.addLine(to: CGPoint(x: 150, y: 300))
-            context.addLine(to: CGPoint(x: 100, y: 350))
-            context.addLine(to: CGPoint(x: 50, y: 300))
-            context.closePath()
-            context.strokePath()
+            //context.move(to: CGPoint(x: 100, y: 250))
+            //context.addLine(to: CGPoint(x: 150, y: 300))
+            //context.addLine(to: CGPoint(x: 100, y: 350))
+            //context.addLine(to: CGPoint(x: 50, y: 300))
+            //context.closePath()
+            //context.strokePath()
             
-            // Drawing an arc
-            context.addArc(center: CGPoint(x: 250, y: 300),
-                           radius: 50,
-                           startAngle: 0,
-                           endAngle: CGFloat(M_PI),
-                           clockwise: true)
-            context.strokePath()
+            
             
             
             // Drawing text
